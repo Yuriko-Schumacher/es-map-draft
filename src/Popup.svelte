2a8103c26c473d490a14faa7948f3c038f20f44c
Yuriko Schumacher <yuriko.schumacher@gmail.com>
2022-02-13 15:26:29 -0500

    more updates

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -2,12 +2,26 @@
   export let data;
 </script>
 
-<h2>{data.district}</h2>
-<div class="summary"><p>{@html data.summary}</p></div>
-<div class="mini-map"></div>
-<div class="donut-chart"></div>
+<div class="container">
+  <div class="left">
+    <h2>{data.district}</h2>
+    <div class="summary"><p>{@html data.summary}</p></div>
+  </div>
+  <div class="right">
+    <div class="mini-map"></div>
+    <div class="donut-chart"></div>
+  </div>
+</div>
 <div class="study"></div>
 
 <style>
-
+  div.container {
+    display: flex;
+  }
+  div.container > div {
+    width: 50%;
+  }
+  div.right {
+    background: turquoise;
+  }
 </style>
