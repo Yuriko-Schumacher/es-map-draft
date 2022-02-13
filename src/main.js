4a3cf313e6ff9ea968fef209f5de128e3e905385
Yuriko Schumacher <yuriko.schumacher@gmail.com>
2022-02-13 13:05:09 -0500

    update liks

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 import App from './App.svelte';
 
-const app = new App({
-  target: document.body,
-  props: {
-  }
-});
-
-// function passCheck(){
-//   let password = prompt("Please enter your password")
-//   if (password == "globeesmap") {
-//     const app = new App({
-//       target: document.body,
-//       props: {
-//       }
-//     });
-//   } else {
-//     alert("Password is incorrect.")
-//     passCheck()
+// const app = new App({
+//   target: document.body,
+//   props: {
 //   }
-// }
-// passCheck();
+// });
+
+function passCheck(){
+  let password = prompt("Please enter your password")
+  if (password == "globeesmap") {
+    const app = new App({
+      target: document.body,
+      props: {
+      }
+    });
+  } else {
+    alert("Password is incorrect.")
+    passCheck()
+  }
+}
+passCheck();
 
 export default app;
