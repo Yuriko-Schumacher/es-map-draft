940dd137f34895e0936444577c932a2141811c35
Yuriko Schumacher <yuriko.schumacher@gmail.com>
2022-02-14 15:33:59 -0500

    apply edits

--- src/Credit.svelte
+++ src/Credit.svelte
@@ -1,5 +1,5 @@
 <span>
-  <em><strong>Sources:</strong> </em>Globe Magazine<em> research; New York University’s Metropolitan Center for Research on Equity and the Transformation of Schools and its <a href="https://crehub.org/take-action" target="_blank">Culturally Responsive Education (CRE) HUB</a>; <a href="https://neaedjustice.org/2021/01/22/movement-grows-to-require-ethnic-studies-in-public-high-schools-despite-california-veto/" target="_blank">National Education Association</a>;</em> <a href="https://rethinkingschools.org/books/rethinking-ethnic-studies/" target="_blank">Rethinking Ethnic Studies</a>.<br>
+  <em><strong>Sources:</strong> </em>Globe Magazine <em> research; New York University’s Metropolitan Center for Research on Equity and the Transformation of Schools and its <a href="https://crehub.org/take-action" target="_blank">Culturally Responsive Education (CRE) HUB</a>; <a href="https://neaedjustice.org/2021/01/22/movement-grows-to-require-ethnic-studies-in-public-high-schools-despite-california-veto/" target="_blank">National Education Association</a>;</em> <a href="https://rethinkingschools.org/books/rethinking-ethnic-studies/" target="_blank">Rethinking Ethnic Studies</a>.<br>
 <em><strong>Map by Yuriko Schumacher</strong></em>
 </span>
 
